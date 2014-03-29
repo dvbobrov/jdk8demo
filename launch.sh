b07a081c80e1b8e4c9d244a2e23dc8300fd4eb3a
Dmitry Bobrov <blackhawk128@gmail.com>
2014-03-30 01:55:48 +0400

    modified launch.sh

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/dev-tools/jdk1.8.0/bin/java -XX:-RestrictContended -jar build/libs/microbenchmarks.jar ".*${1}.*"
+"${JAVA_HOME}/bin/java" -XX:-RestrictContended -jar build/libs/microbenchmarks.jar ".*${1}.*"
